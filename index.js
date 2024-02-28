866cadadf06807452624c08d774fb529b7f1e72c
Bikal Rai <bikalrai2785@gmailcom>
2024-02-28 18:43:25 +0545

    add addBook page,render button based on page and make footer sticky bottom

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 const db = new pg.Client({
   user: 'postgres',
   host: 'localhost',
-  database: '',
+  database: 'book_db',
   password: '123456',
   port: 5432,
 });
@@ -19,7 +19,26 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  // currentPage value to be passed to the ejs
+  res.render('index.ejs', { currentPage: 'home' });
+});
+
+app.get('/addBook', (req, res) => {
+  // get the value of the button
+  const back = req.query.return;
+
+  // return back to homepage when clicking on the back button
+  if (back === 'back') {
+    res.redirect('/');
+  }
+  // currentPage value to be passed to the ejs
+  res.render('addBook.ejs', { currentPage: 'add' });
+});
+
+app.post('/add', (req, res) => {
+  const title = req.body.title;
+  const desc = req.body.decs;
+  const isbn = req.body.isbn;
 });
 
 app.listen(port, () => {
